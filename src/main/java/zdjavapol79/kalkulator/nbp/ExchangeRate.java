360b783859a9ff77f8de2cb87543c349612ce020
Patryk <patryk.marcisz.v2@gmail.com>
2021-05-24 18:44:28 +0200

    first mapping

---
+++ src/main/java/zdjavapol79/kalkulator/nbp/ExchangeRate.java
@@ -0,0 +1,43 @@
+package zdjavapol79.kalkulator.nbp;
+
+import java.util.List;
+
+public class ExchangeRate {
+
+    private String table;
+    private String no;
+    private String effectiveDate;
+    private List<Rate> rates;
+
+    public String getTable() {
+        return table;
+    }
+
+    public void setTable(String table) {
+        this.table = table;
+    }
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+    public String getEffectiveDate() {
+        return effectiveDate;
+    }
+
+    public void setEffectiveDate(String effectiveDate) {
+        this.effectiveDate = effectiveDate;
+    }
+
+    public List<Rate> getRates() {
+        return rates;
+    }
+
+    public void setRates(List<Rate> rates) {
+        this.rates = rates;
+    }
+}
