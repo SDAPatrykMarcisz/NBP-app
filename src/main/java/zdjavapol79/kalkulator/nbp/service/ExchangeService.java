d931dab8db5e84f3b619feb6a6199f0ca004f98c
Patryk <patryk.marcisz.v2@gmail.com>
2021-05-24 20:58:52 +0200

    first method of service

---
+++ src/main/java/zdjavapol79/kalkulator/nbp/service/ExchangeService.java
@@ -0,0 +1,33 @@
+package zdjavapol79.kalkulator.nbp.service;
+
+import zdjavapol79.kalkulator.nbp.model.dto.ExchangeRate;
+import zdjavapol79.kalkulator.nbp.model.dto.Rate;
+import zdjavapol79.kalkulator.nbp.repository.NBPClient;
+
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public class ExchangeService {
+
+    private NBPClient nbpClient;
+
+    public ExchangeService(){
+        this.nbpClient = new NBPClient();
+    }
+
+    public Double convert(String currencyCodeFrom, String currencyCodeTo, Double amount){
+        Optional<Double> from = getMidForCurrency(currencyCodeFrom);
+        Optional<Double> to = getMidForCurrency(currencyCodeTo);
+        throw new UnsupportedOperationException("not implemented yet");
+    }
+
+    private Optional<Double> getMidForCurrency(String currencyCodeFrom) {
+        return Stream.of(nbpClient.downloadExchangeRates())
+                .flatMap(exchangeRate -> exchangeRate.getRates().stream())
+                .filter(x -> x.getCode().equals(currencyCodeFrom))
+                .map(x -> x.getMid())
+                .findFirst();
+    }
+
+}
